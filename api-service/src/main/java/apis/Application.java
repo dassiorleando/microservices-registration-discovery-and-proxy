42ce002095b7c5264544f4beb57d3076c8bc85b6
dassiorleando <dassi_orleando@yahoo.fr>
2016-05-18 14:24:42 +0100

    https://github.com/joshlong/service-registration-and-discovery + consultation-service and a zuul server

---
+++ api-service/src/main/java/apis/Application.java
@@ -0,0 +1,15 @@
+package apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+// @EnableDiscoveryClient
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
